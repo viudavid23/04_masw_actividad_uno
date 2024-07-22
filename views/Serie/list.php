3478234bccf50d99acac6ba52a0e3f8ca5149101
viudavid23 <137242113+viudavid23@users.noreply.github.com>
2024-07-21 23:18:55 -0500

    Add modal confirmation and new detail column in serie view

--- views/Serie/list.php
+++ views/Serie/list.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php require_once '../../utils/SessionStart.php'; ?>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <title>Series</title>
@@ -9,9 +10,9 @@
 
 <body>
     <div class="container">
-        
-    <?php include('../../menu.php'); ?>
-        
+
+        <?php include('../../menu.php'); ?>
+
         <div class="col-12">
             <h2 class="h2">Listado de Series</h2>
             <div class="btn-group mb-3" role="group" aria-label="Buttons Area">
@@ -50,16 +51,36 @@
                                     <td><?php echo $serie->getTitle(); ?></td>
                                     <td><?php echo $serie->getSynopsis(); ?></td>
                                     <td><?php echo $serie->getReleaseDate(); ?></td>
-                                    <td></td>
+                                    <td><a href="edit.php?id=<?php echo $serie->getId(); ?>"><?php echo $serie->getId(); ?></a></td>
                                     <td>
                                         <div class="btn-group" role="group" aria-label="Buttons Area">
                                             <a class="btn btn-success" href="edit.php?id=<?php echo $serie->getId(); ?>">Editar</a>
                                         </div>
                                         <div class="btn-group" role="group" aria-label="Buttons Area">
-                                            <form name="delete_serie" action="delete.php" method="POST">
-                                                <input type="hidden" name="serieId" value="<?php echo $serie->getId(); ?>" />
-                                                <button type="submit" class="btn btn-danger">Eliminar</button>
-                                            </form>
+                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteConfirmModal">Eliminar</button>
+                                            <!-- Modal Confirmación-->
+                                            <div class="modal fade" id="deleteConfirmModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="modalTextLabel">Eliminación de Serie <strong><?php echo $serie->getTitle(); ?></strong></h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Está acción eliminará la serie de todas las plataformas junto con sus actores/actrices e idiomas de audio y subtitulos.
+                                                            ¿Desea continuar?
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+
+                                                            <form name="delete_serie" action="delete.php" method="POST">
+                                                                <input type="hidden" name="serieId" value="<?php echo $serie->getId(); ?>" />
+                                                                <button type="submit" class="btn btn-primary">Confirmar</button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </td>
                                 </tr>
