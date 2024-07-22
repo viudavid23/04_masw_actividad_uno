aa540bb4c6bd917c27a694d78b75304d5be548df
viudavid23 <137242113+viudavid23@users.noreply.github.com>
2024-07-21 23:08:21 -0500

    inc new control message, arraytoupdate(experimental) and concat strings functions

--- utils/Utilities.php
+++ utils/Utilities.php
@@ -3,7 +3,7 @@ class Utilities
 {
 
     /**
-     * Funcion que permite cambiar el formato de una fecha
+     * Función que permite cambiar el formato de una fecha
      * @param String $fechaSinFormato fecha a ser formateada
      * @param string $formato formato de la fecha. Se encuentran en la clase Constants.php
      * 
@@ -20,7 +20,7 @@ class Utilities
     }
 
     /**
-     * Funcion que permite convertir los caracteres de minusculas a mayusculas y viceversa incluyendo caracteres acentuados
+     * Función que permite convertir los caracteres de minusculas a mayusculas y viceversa incluyendo caracteres acentuados
      * @param int $option 0 para convertir de mayusculas a minusculas y 1 para convertir de minusculas a mayusculas
      * @param string $text cadena de texto a ser transformada
      * 
@@ -35,5 +35,66 @@ class Utilities
 
         return $option == 0 ? str_replace($uppercase, $lowerCase, strtolower($text)) :
             str_replace($lowerCase, $uppercase, strtolower($text));
+    }
+
+    /**
+     * Función que permite el seteo de un mensaje de error en una variable de sesion
+     * @param string $message mensaje de error customizado
+     */
+    public static function setErrorMessage($message)
+    {
+        $_SESSION['error_message'] = $message;
+    }
+
+    /**
+     * Función que permite el seteo de un mensaje de éxito en una variable de sesion
+     * @param string $message mensaje de éxito customizado
+     */
+    public static function setSuccessMessage($message)
+    {
+        $_SESSION['success_message'] = $message;
+    }
+
+    /**
+     * Función que permite el seteo de un mensaje de advertencia en una variable de sesion
+     * @param string $message mensaje de advertencia customizado
+     */
+    public static function setWarningMessage($message)
+    {
+        $_SESSION['warning_message'] = $message;
+    }
+
+    /**
+     * Función que permite identificar los elementos existentes y no existentes dentro de una lista de
+     * ids a actualizar discriminados por un estado
+     * @param array $newObjectIds arreglo de ids nuevos
+     * @param array $objectIdsSaved arreglo de ids actuales en la base de datos
+     * 
+     * @return array $objectToUpdate con los elementos a actualizar. La llave corresponde al id
+     * del objeto y el valor a su nuevo estado
+     */
+    public static function setArrayToUpdate(array $newObjectIds, array $objectIdsSaved): array
+    {
+        $objectToUpdate = [];
+        foreach ($objectIdsSaved as $currentObjectItem) {
+
+            $objectToUpdate[$currentObjectItem] = in_array($currentObjectItem, $newObjectIds) ? 1 : 0;
+        }
+
+        return $objectToUpdate;
+    }
+
+    /**
+     * Función para concatenar strings.
+     */
+    public static function concatStrings() {
+        $args = func_get_args();
+        $result = '';
+    
+        foreach ($args as $arg) {
+            $result .= $arg;
+        }
+    
+        return $result;
     }
 }
