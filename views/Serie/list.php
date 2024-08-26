8f3ca526fb8cfa5b72edecdc0de63874645164a1
viudavid23 <josedavid.mejia@alumnos.viu.es>
2024-08-25 23:22:19 -0500

    adding modal delete confirmation in list serie view

--- views/Serie/list.php
+++ views/Serie/list.php
@@ -46,6 +46,7 @@
                                 $serie = $serieController->showById($serie->getId());
                                 $serie->setReleaseDate(Utilities::changeDateFormat($serie->getReleaseDate(), Constants::DATE_OUTPUT_FORMAT));
                                 $uniqueModalId = "deleteSerieModal" . $serie->getId();
+                                $platformsModalId = "seriesPlatformModal" . $serie->getId();
                             ?>
                                 <tr>
                                     <td><?php echo $serie->getId(); ?></td>
@@ -58,10 +59,9 @@
                                             <a class="btn btn-success" href="edit.php?id=<?php echo $serie->getId(); ?>">Editar</a>
                                         </div>
                                         <div class="btn-group" role="group" aria-label="Buttons Area">
-                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#<?php echo $uniqueModalId; ?>">Eliminar</button>
                                             <!-- Modal Confirmación-->
-                                            <div class="modal fade" id="<?php echo $uniqueModalId; ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
+                                            <div class="modal fade" id="<?php echo $uniqueModalId; ?>" aria-hidden="true" aria-labelledby="staticBackdropLabel" tabindex="-1">
+                                                <div class="modal-dialog modal-dialog-centered">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
                                                             <h5 class="modal-title" id="modalTextLabel">Eliminación de Serie <strong><?php echo $serie->getTitle(); ?></strong></h5>
@@ -71,17 +71,47 @@
                                                             Está acción eliminará la serie de todas las plataformas junto con sus actores/actrices e idiomas de audio y subtitulos.
                                                             ¿Desea continuar?
                                                         </div>
+
                                                         <div class="modal-footer">
+
                                                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
 
                                                             <form name="delete_serie" action="delete.php" method="POST">
                                                                 <input type="hidden" name="serieId" value="<?php echo $serie->getId(); ?>" />
                                                                 <button type="submit" class="btn btn-primary">Confirmar</button>
                                                             </form>
+                                                            <button class="btn btn-info" data-bs-target="#<?php echo $platformsModalId; ?>" data-bs-toggle="modal" data-bs-dismiss="modal">Plataformas Asociadas</button>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal fade" id="<?php echo $platformsModalId; ?>" aria-hidden="true" aria-labelledby="platformSeriesLabel" tabindex="-1">
+                                                <div class="modal-dialog modal-dialog-centered">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="platformSeriesLabel">Plataformas de la serie <strong><?php echo $serie->getTitle(); ?></strong></h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <ul class="list-group list-group-flush">
+                                                                <?php
+                                                                require_once '../../controllers/PlatformSerieController.php';
+
+                                                                $platformSerieController = new PlatformSerieController();
+                                                                $seriesPlatformOptions = $platformSerieController->getPlatformListBySerie($serie->getId());
+
+                                                                echo $seriesPlatformOptions;
+                                                                ?>
+                                                            </ul>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button class="btn btn-primary" data-bs-target="#<?php echo $uniqueModalId; ?>" data-bs-toggle="modal" data-bs-dismiss="modal">Regresar</button>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
+                                            <a class="btn btn-danger" data-bs-toggle="modal" href="#<?php echo $uniqueModalId; ?>" role="button">Eliminar</a>
                                         </div>
                                     </td>
                                 </tr>
