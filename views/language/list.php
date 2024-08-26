f3bc63d311e1b584e4680f1068f4e12c697fa65b
viudavid23 <josedavid.mejia@alumnos.viu.es>
2024-08-25 23:20:30 -0500

    Adding modal delete in language view and series validation

--- views/language/list.php
+++ views/language/list.php
@@ -41,43 +41,79 @@
                             <?php
                             foreach ($languageList as $item) {
                                 $uniqueModalId = "deleteLanguageModal" . $item->getId();
+                                $languageSeriesModalId = "languageSerieModal" . $item->getId();
                             ?>
                                 <tr>
                                     <td><?php echo $item->getId() ?></td>
                                     <td><?php echo $item->getName() ?></td>
                                     <td><?php echo $item->getIsoCode() ?></td>
+                                    <?php
+                                    if ($item->getId() != 1) {
+                                    ?>
+                                        <td>
+                                            <div class="btn-group" role="group" aria-label="Buttons Area">
+                                                <a class="btn btn-success" href="edit.php?id=<?php echo $item->getId(); ?>">Editar</a>
+                                            </div>
+                                            <div class="btn-group" role="group" aria-label="Buttons Area">
 
-                                    <td>
-                                        <div class="btn-group" role="group" aria-label="Buttons Area">
-                                            <a class="btn btn-success" href="edit.php?id=<?php echo $item->getId(); ?>">Editar</a>
-                                        </div>
-                                        <div class="btn-group" role="group" aria-label="Buttons Area">
-                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#<?php echo $uniqueModalId; ?>">Eliminar</button>
-                                            <!-- Modal Confirmación-->
-                                            <div class="modal fade" id="<?php echo $uniqueModalId; ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h5 class="modal-title" id="modalTextLabel">Eliminación de Idioma <strong><?php echo $item->getName(); ?></strong></h5>
-                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            Está acción eliminará el idioma y sus relaciones con las series.
-                                                            ¿Desea continuar?
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                                <!-- Modal Confirmación-->
+                                                <div class="modal fade" id="<?php echo $uniqueModalId; ?>" aria-hidden="true" aria-labelledby="staticBackdropLabel" tabindex="-1">
+                                                    <div class="modal-dialog modal-dialog-centered">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="modalTextLabel">Eliminación de Idioma <strong><?php echo $item->getName(); ?></strong></h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                Está acción eliminará el idioma y sus relaciones con las series.
+                                                                ¿Desea continuar?
+                                                            </div>
 
-                                                            <form name="delete_language" action="delete.php" method="POST">
-                                                                <input type="hidden" name="languageId" value="<?php echo $item->getId(); ?>" />
-                                                                <button type="submit" class="btn btn-primary">Confirmar</button>
-                                                            </form>
+                                                            <div class="modal-footer">
+
+                                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+
+                                                                <form name="delete_language" action="delete.php" method="POST">
+                                                                    <input type="hidden" name="languageId" value="<?php echo $item->getId(); ?>" />
+                                                                    <button type="submit" class="btn btn-primary">Confirmar</button>
+                                                                </form>
+                                                                <button class="btn btn-info" data-bs-target="#<?php echo $languageSeriesModalId; ?>" data-bs-toggle="modal" data-bs-dismiss="modal">Series Asociadas</button>
+                                                            </div>
+
                                                         </div>
                                                     </div>
                                                 </div>
+                                                <div class="modal fade" id="<?php echo $languageSeriesModalId; ?>" aria-hidden="true" aria-labelledby="platformSeriesLabel" tabindex="-1">
+                                                    <div class="modal-dialog modal-dialog-centered">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title" id="platformSeriesLabel">Series en Idioma <strong><?php echo $item->getName(); ?></strong></h5>
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                <ul class="list-group list-group-flush">
+                                                                    <?php
+                                                                    require_once '../../controllers/LanguageSerieController.php';
+
+                                                                    $languageSerieController = new LanguageSerieController();
+                                                                    $languageseriesOptions = $languageSerieController->getSerieListByLanguage($item->getId());
+
+                                                                    echo $languageseriesOptions;
+                                                                    ?>
+                                                                </ul>
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button class="btn btn-primary" data-bs-target="#<?php echo $uniqueModalId; ?>" data-bs-toggle="modal" data-bs-dismiss="modal">Regresar</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <a class="btn btn-danger" data-bs-toggle="modal" href="#<?php echo $uniqueModalId; ?>" role="button">Eliminar</a>
                                             </div>
-                                        </div>
-                                    </td>
+                                        </td>
+                                    <?php
+                                    }
+                                    ?>
                                 </tr>
                             <?php
                             }
